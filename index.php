152b9dbc71a4828d672187d468e5a7155af1f83a
Mark Coker <markcoker@users.noreply.github.com>
2020-05-07 00:01:59 +1000

    upgrade bootstrap attempt

--- index.php
+++ index.php
@@ -15,7 +15,8 @@
 		<!-- Bootstrap -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-        <!-- rsyslog-webui css -->
+        <script src="js/bootstrap-context.js"></script> 
+		<!-- rsyslog-webui css -->
 		<link href="css/custom.css" rel="stylesheet">
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media
         queries -->
