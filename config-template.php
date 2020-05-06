12f402950516a0a63fbe03ac839aa50eb562fe19
mark@cokers.com.au <mark@cokers.com.au>
2020-05-06 22:59:59 +1000

    Add config template

---
+++ config-template.php
@@ -0,0 +1,17 @@
+<?php
+
+	date_default_timezone_set( "Australia/Sydney" );
+
+	// Mysql syslog database config
+	$mysql_server = "127.0.0.1";
+	$mysql_database = "";
+	$mysql_user = "";
+	$mysql_password = "";
+
+	// Site Settings
+	$site_name = "System Logs";
+
+	// Database Settings
+	$keep_logs_for_days = 90;
+
+?>
