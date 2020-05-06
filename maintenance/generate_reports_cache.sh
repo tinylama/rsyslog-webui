af04da6905e0b176cad9735ac9eb60858ba37b4f
mark@cokers.com.au <mark@cokers.com.au>
2020-05-06 22:55:58 +1000

    remove broken features

--- maintenance/generate_reports_cache.sh
+++
@@ -1,5 +0,0 @@
-
-cd ../json/reports
-php report_amchart_severity_days_overview.php > cache/report_amchart_severity_days_overview.json
-php report_amchart_severity_overview.php > cache/report_amchart_severity_overview.json
-php report_amchart_severity_overview_pie.php > cache/report_amchart_severity_overview_pie.json
